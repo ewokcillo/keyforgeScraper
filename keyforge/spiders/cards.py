54a680f0121a334b79aa81a4ae30513c80b4da5b
Diego Pascual <diego.pascual@openshine.com>
2018-12-14 15:29:30 +0100

    Added restart at the end of the last deck scraped

--- keyforge/spiders/cards.py
+++ keyforge/spiders/cards.py
@@ -1,21 +1,35 @@
 # -*- coding: utf-8 -*-
+import pymongo
 import scrapy
 from scrapy import Request
 
 import json
 import urllib.parse as urlparse
 
+import keyforge.settings
+
+
 PAGE_SIZE = 10
 PAGE_URL = ("https://www.keyforgegame.com/api/decks/?"
             "page={}&page_size=10&search=&power_level=0,11"
-            "&chains=0,24&ordering=-date")
+            "&chains=0,24&ordering=date")
 DECK_URL = 'https://www.keyforgegame.com/api/decks/{}/?links=cards,notes'
+
+
+def get_first_page():
+    mongo_uri = keyforge.settings.MONGO_URI,
+    mongo_db = keyforge.settings.MONGO_DATABASE
+    client = pymongo.MongoClient(mongo_uri)
+    db = client[mongo_db]
+    page = int(db['decks'].count()/PAGE_SIZE) - 1
+
+    return page if page > 0 else 1
 
 
 class CardsSpider(scrapy.Spider):
     name = 'cards'
     allowed_domains = ['keyforgegame.com']
-    start_urls = [PAGE_URL.format(1, PAGE_SIZE)]
+    start_urls = [PAGE_URL.format(get_first_page(), PAGE_SIZE)]
 
     def parse(self, response):
         page = int(urlparse.parse_qs(response.url.split('?')[-1])['page'][-1])
